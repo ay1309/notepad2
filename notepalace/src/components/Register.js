b9c1673b21f2b5aa306233e32f9090e3b0174f93
Selex9034e <selenejuarezt@gmail.com>
2024-06-20 01:09:42 -0600

    registrer

--- notepalace/src/components/Register.js
+++ notepalace/src/components/Register.js
@@ -1,8 +1,27 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import fondoReflejo from './fondoReflejo.jpg';
 
 const Register = () => {
+  const backgroundStyle = {
+    backgroundImage: `url(${fondoReflejo})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+  };
+  
+  const boxStyle = {
+    backgroundColor: 'rgba(255, 255, 255, 0.8)', // Blanco semitransparente
+    padding: '20px',
+    borderRadius: '10px',
+  };
+
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -25,24 +44,26 @@ const Register = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleRegister}>
-        <h2>Registrarse</h2>
-        <label>
-          Nombre:
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
-        </label>
-        <label>
-          Email:
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-        </label>
-        <label>
-          Contraseña:
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-        </label>
-        <button type="submit">Registrarse</button>
-      </form>
-      <button onClick={handleHomeRedirect}>Regresar</button>
+    <div style={backgroundStyle}>
+      <div style={boxStyle}>
+       <form onSubmit={handleRegister}>
+         <h2>Registrarse</h2>
+         <label>
+           Nombre:
+           <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+         </label>
+         <label>
+           Email:
+           <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+         </label>
+         <label>
+           Contraseña:
+           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+         </label>
+         <button type="submit">Registrarse</button>
+       </form>
+       <button onClick={handleHomeRedirect}>Regresar</button>
+      </div> 
     </div>
   );
 };
