692410464bfb3b78f132fd51ceb3bb35727dcc23
Selex9034e <selenejuarezt@gmail.com>
2024-06-27 08:54:17 -0600

    FOOTER

--- notepalace/src/components/Footer.js
+++ notepalace/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer>
+    <footer style={{ backgroundColor: 'transparent', color: 'black', padding: '10px', textAlign: 'center' }}>
       <p>&copy; 2024 NotePalace. </p>
         <p>Todos los derechos reservados.</p>
     </footer>
